3b575558fef1fb72826461ffb619a8ce606cb181
Maxence <Maxencemenard@hotmail.fr>
2022-01-02 23:19:45 +0100

    ajout de maker-bundle / orm et creation des entité

---
+++ src/Entity/Location.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LocationRepository::class)]
+class Location
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $id_locataire;
+
+    #[ORM\Column(type: 'integer')]
+    private $id_residence;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $fichier_inventaire;
+
+    #[ORM\Column(type: 'datetime')]
+    private $date_arriver;
+
+    #[ORM\Column(type: 'datetime')]
+    private $date_depart;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $commentaire_locataire;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $signature_locataire;
+
+    #[ORM\Column(type: 'datetime')]
+    private $date_validation_locataire;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $commentaire_mandataire;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $signature_mandataire;
+
+    #[ORM\Column(type: 'datetime')]
+    private $date_validation_mandataire;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdLocataire(): ?int
+    {
+        return $this->id_locataire;
+    }
+
+    public function setIdLocataire(int $id_locataire): self
+    {
+        $this->id_locataire = $id_locataire;
+
+        return $this;
+    }
+
+    public function getIdResidence(): ?int
+    {
+        return $this->id_residence;
+    }
+
+    public function setIdResidence(int $id_residence): self
+    {
+        $this->id_residence = $id_residence;
+
+        return $this;
+    }
+
+    public function getFichierInventaire(): ?string
+    {
+        return $this->fichier_inventaire;
+    }
+
+    public function setFichierInventaire(string $fichier_inventaire): self
+    {
+        $this->fichier_inventaire = $fichier_inventaire;
+
+        return $this;
+    }
+
+    public function getDateArriver(): ?\DateTimeInterface
+    {
+        return $this->date_arriver;
+    }
+
+    public function setDateArriver(\DateTimeInterface $date_arriver): self
+    {
+        $this->date_arriver = $date_arriver;
+
+        return $this;
+    }
+
+    public function getDateDepart(): ?\DateTimeInterface
+    {
+        return $this->date_depart;
+    }
+
+    public function setDateDepart(\DateTimeInterface $date_depart): self
+    {
+        $this->date_depart = $date_depart;
+
+        return $this;
+    }
+
+    public function getCommentaireLocataire(): ?string
+    {
+        return $this->commentaire_locataire;
+    }
+
+    public function setCommentaireLocataire(string $commentaire_locataire): self
+    {
+        $this->commentaire_locataire = $commentaire_locataire;
+
+        return $this;
+    }
+
+    public function getSignatureLocataire(): ?string
+    {
+        return $this->signature_locataire;
+    }
+
+    public function setSignatureLocataire(string $signature_locataire): self
+    {
+        $this->signature_locataire = $signature_locataire;
+
+        return $this;
+    }
+
+    public function getDateValidationLocataire(): ?\DateTimeInterface
+    {
+        return $this->date_validation_locataire;
+    }
+
+    public function setDateValidationLocataire(\DateTimeInterface $date_validation_locataire): self
+    {
+        $this->date_validation_locataire = $date_validation_locataire;
+
+        return $this;
+    }
+
+    public function getCommentaireMandataire(): ?string
+    {
+        return $this->commentaire_mandataire;
+    }
+
+    public function setCommentaireMandataire(string $commentaire_mandataire): self
+    {
+        $this->commentaire_mandataire = $commentaire_mandataire;
+
+        return $this;
+    }
+
+    public function getSignatureMandataire(): ?string
+    {
+        return $this->signature_mandataire;
+    }
+
+    public function setSignatureMandataire(string $signature_mandataire): self
+    {
+        $this->signature_mandataire = $signature_mandataire;
+
+        return $this;
+    }
+
+    public function getDateValidationMandataire(): ?\DateTimeInterface
+    {
+        return $this->date_validation_mandataire;
+    }
+
+    public function setDateValidationMandataire(\DateTimeInterface $date_validation_mandataire): self
+    {
+        $this->date_validation_mandataire = $date_validation_mandataire;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Residence.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ResidenceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ResidenceRepository::class)]
+class Residence
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $nom;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $adresse;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $ville;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $code_postal;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $pays;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $fichier_inventaire;
+
+    #[ORM\Column(type: 'integer')]
+    private $id_bailleur;
+
+    #[ORM\Column(type: 'integer')]
+    private $id_mandataire;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->code_postal;
+    }
+
+    public function setCodePostal(string $code_postal): self
+    {
+        $this->code_postal = $code_postal;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->pays;
+    }
+
+    public function setPays(string $pays): self
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+
+    public function getFichierInventaire(): ?string
+    {
+        return $this->fichier_inventaire;
+    }
+
+    public function setFichierInventaire(string $fichier_inventaire): self
+    {
+        $this->fichier_inventaire = $fichier_inventaire;
+
+        return $this;
+    }
+
+    public function getIdBailleur(): ?int
+    {
+        return $this->id_bailleur;
+    }
+
+    public function setIdBailleur(int $id_bailleur): self
+    {
+        $this->id_bailleur = $id_bailleur;
+
+        return $this;
+    }
+
+    public function getIdMandataire(): ?int
+    {
+        return $this->id_mandataire;
+    }
+
+    public function setIdMandataire(int $id_mandataire): self
+    {
+        $this->id_mandataire = $id_mandataire;
+
+        return $this;
+    }
+}
