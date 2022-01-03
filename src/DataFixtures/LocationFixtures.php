d06e58e77c83fce22664fa2a309ce9b90c56df43
Eadgeez <52535495+Eadgeez@users.noreply.github.com>
2022-01-03 15:51:27 +0100

    Ajout des fixtures.

---
+++ src/DataFixtures/LocationFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Location;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class LocationFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        //création d'un timestamp aléatoire
+        $timestamp = mt_rand(1, time());
+        $date = new \DateTime("2009-04-10");
+        $date->setTimestamp($timestamp);
+
+        for ($i = 0; $i < 20; $i++) {
+            $location = new Location();
+            $location->setFichierInventaire("FichierInv-".$i.".pdf");
+            $location->setDateArriver($date);
+            $location->setDateDepart($date);
+            $location->setCommentaireLocataire("c'est super !");
+            $location->setSignatureLocataire("signatureL");
+            $location->setDateValidationLocataire($date);
+            $location->setCommentaireMandataire("parfait !");
+            $location->setSignatureMandataire("signatureM");
+            $location->setDateValidationMandataire($date);
+            $location->setIdLocataire($this->getReference('user-'.rand(1,5)));
+            $location->setIdResidence($this->getReference('Residence-'.rand(1,5)));
+            $manager->persist($location);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            UserFixtures::class,
+            ResidenceFixtures::class,
+        ];
+    }
+
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $user = new user();
+        // $manager->persist($user);
+
+        $roles = array("Locataire", "Mandataire", "Bailleur");
+
+        for ($i = 0; $i < 20; $i++) {
+            $user = new User();
+            $user->setRole($roles[rand(0,2)]);
+            $user->setEmail("mail@gmail.com");
+            $user->setMotDePasse("motdepasse1234");
+            $user->setEstVerifiée(rand(0,1)==1);
+            $manager->persist($user);
+            $this->addReference('user-'.$i, $user);
+        }
+
+        $manager->flush();
+    }
+}
