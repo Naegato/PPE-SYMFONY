1ff3aea9d8788b7dd14db6b7a3b51ac47b0f69fa
Naegato <maxime.wiatr@gmail.com>
2022-01-04 16:46:55 +0100

    template connexion

--- public/js/script.js
+++ public/js/script.js
@@ -6,35 +6,14 @@
         this.classList.toggle("active")
         if (this.classList.contains('active')){
             for (let i=0; i<toggleItems.length;i++){
-                // console.log(toggleItems[i].style)
                 toggleItems[i].classList.add('active')
             }
         }else{
             for (let i=0; i<toggleItems.length;i++){
-                // console.log(toggleItems[i].style)
                 toggleItems[i].classList.remove('active')
             }
         }
     })
 
-    window.addEventListener("resize",function (){
-        // console.log(this.innerWidth);
-        if (this.innerWidth > 912){
-            button.classList.remove('active')
-            for (let i=0;i<toggleItems.length;i++){
-                toggleItems[i].classList.remove('active')
-                // toggleItems[i].style.position = "relative"
-            }
-        }else{
-            let offset=0;
-            for (let i = 0; i < toggleItems.length; i++) {
-                // toggleItems[i].style.position = "absolute"
-                console.log(toggleItems[i])
-                console.log(toggleItems[i].getBoundingClientRect())
-                offset += toggleItems[i].getBoundingClientRect().height
-                console.log(offset)
-                toggleItems[i].style.bottom = (-1 * offset)
-            }
-        }
-    })
+
 })()
